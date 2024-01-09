c5a40a143ad0f0090dd308e31867561acf7bd4d2
nancy-konadu <nancykyiadom994@gmail.com>
2024-01-09 08:38:30 -0800

    Input output update

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Reads from standard input and computes metrics
+"""Reads from standard input and computes metrics.
 """
 
 
 def print_stats(size, status_codes):
-    """Print accumulated metrics
+    """Print accumulated metrics.
+
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
     """
     print("File size: {}".format(size))
     for key in sorted(status_codes):
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
+"""a script that adds all arguments to a Python list
+and then save them to a file
+"""
 
 
 import sys
